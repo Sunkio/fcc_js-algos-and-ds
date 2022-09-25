158af9c24d667272e3065723c67f2980dc8db35c
Tanja Schmidt <tagretha@gmail.com>
2022-09-25 09:01:36 +0200

    refactor: palindrome checker

--- 1_palindrome-checker/palindrom-checker.js
+++ 1_palindrome-checker/palindrom-checker.js
@@ -1,21 +1,25 @@
+function reverseStr (workStr) {
+    let revStr = "";
+    for (let i = workStr.length - 1; i >= 0; i--) {
+     revStr += workStr[i];
+  }
+  return revStr;
+}
+
 function palindrome(str) {
-  let workString = "";
-  let revWorkStr = "";
-  const regExp = /[a-z0-9]/i;
-  for(let i = 0; i < str.length; i++) {
-    if (regExp.test(str[i])) {
-      workString += str[i];
-    }
-  }
-  workString = workString.toLowerCase();
-  for (let i = workString.length - 1; i >= 0; i--) {
-    revWorkStr += workString[i];
-  }
-  if(workString == revWorkStr) {
+  let workStr = "";
+  let revStr = "";
+  const regExp = /[a-z0-9]/ig;
+
+  workStr = str.match(regExp).join("");
+  workStr = workStr.toLowerCase();
+  revStr = reverseStr(workStr);
+  if (workStr === revStr) {
     return true;
   } else {
     return false;
-  } 
+  }
 }
 
 palindrome("eye");
+
